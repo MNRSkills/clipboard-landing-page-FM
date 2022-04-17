92f001f5647df1dbdace12f3983ec59abff222d6
MNR_Dev <mnrdev510@gmail.com>
2022-04-16 21:59:51 -0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*/*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
